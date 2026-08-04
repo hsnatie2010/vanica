f4321a463ce397ff37731c9624d56ae78fc7bba7
hsnatie2010 <hsnatieh2010@gmail.com>
2026-08-04 08:54:51 +0300

    Adjust image size classes for service images

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -365,7 +365,7 @@ export default function HomePage() {
                         <img
                           src={service.image}
                           alt={service.title}
-                          className="w-28.5 h-28.5 object-contain"
+                          className="w-28.3 h-28.3 object-contain"
                         />
                       ) : (
                         <div className="text-6xl">{service.icon}</div>
