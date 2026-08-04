16729b6080734e8897df694377d1434da08281e1
hsnatie2010 <hsnatieh2010@gmail.com>
2026-08-04 09:00:09 +0300

    Fix image class dimensions in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -365,7 +365,7 @@ export default function HomePage() {
                         <img
                           src={service.image}
                           alt={service.title}
-                          className="w-28.1 h-28.1 object-contain"
+                          className="w-28.05 h-28.05 object-contain"
                         />
                       ) : (
                         <div className="text-6xl">{service.icon}</div>
