089b2690da6f80f89e3b4cff0696e6079971ae2d
hsnatie2010 <hsnatieh2010@gmail.com>
2026-08-04 08:58:24 +0300

    Fix image class size in HomePage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -365,7 +365,7 @@ export default function HomePage() {
                         <img
                           src={service.image}
                           alt={service.title}
-                          className="w-28.2 h-28.2 object-contain"
+                          className="w-28.1 h-28.1 object-contain"
                         />
                       ) : (
                         <div className="text-6xl">{service.icon}</div>
